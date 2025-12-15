b8a086c07b5751c56c1d7678576f5f97e6f59432
Lyndon Bredenkamp <bredenkamp@gmail.com>
2025-12-15 18:08:39 +0000

    Final mobile polish: Inline Critical CSS, async app.css, fix SVG error

--- root-supervan-uk/app/routes/home.tsx
+++ root-supervan-uk/app/routes/home.tsx
@@ -72,7 +72,7 @@ export default function Home() {
                   <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
                     <svg className="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                        d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z">
+                        d="M9 12h6m-6 4h6m2 5H7a2 2 0 0 1 -2 -2V5a2 2 0 0 1 2 -2h5.586a1 1 0 0 1 .707 .293l5.414 5.414a1 1 0 0 1 .293 .707V19a2 2 0 0 1 -2 2z">
                       </path>
                     </svg>
                   </div>
